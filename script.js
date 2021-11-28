d929aef7194a207a7654e51d0ad1d8e0e2e1c973
Kiahna Carmack <Kiahna.carmack@gmail.com>
2021-11-28 14:22:39 -0800

    Co-authored-by: Tyler Arthur <tarthurf@yahoo.com>

---
+++ script.js
@@ -0,0 +1,24 @@
+//start the game
+//set the question
+//select answer
+const startButton = document.getElementById('start-btn')
+const questionContainer = document.getElementById('question-container')
+
+function startGame() {
+    console.log('Quiz Has Begun')
+    startButton.classList.add('hide')
+}
+
+ startButton.addEventListener('click', startGame)
+
+ function startGame() {
+    console.log('Quiz Has Begun')
+    startButton.classList.add('hide')
+
+}
+function nextQuestion() {
+
+}
+function selectAnswer() {
+
+}
