eca2b1f48da7b0f91872406726fabbe248d6e980
Kiahna Carmack <Kiahna.carmack@gmail.com>
2021-11-28 23:26:26 -0800

    commit to 'main

--- script.js
+++ script.js
@@ -9,6 +9,41 @@ const questionContainer = document.getElementById('question-container')
 const questionEl = document.getElementById('question')
 const answerEl = document.getElementById('answerBtn')
 
+const questions = [
+    {
+      question: 'What is 2 + 2?',
+      answers: [
+        { text: '4', correct: true },
+        { text: '22', correct: false }
+      ]
+    },
+    {
+      question: 'Who is the best YouTuber?',
+      answers: [
+        { text: 'Web Dev Simplified', correct: true },
+        { text: 'Traversy Media', correct: true },
+        { text: 'Dev Ed', correct: true },
+        { text: 'Fun Fun Function', correct: true }
+      ]
+    },
+    {
+      question: 'Is web development fun?',
+      answers: [
+        { text: 'Kinda', correct: false },
+        { text: 'YES!!!', correct: true },
+        { text: 'Um no', correct: false },
+        { text: 'IDK', correct: false }
+      ]
+    },
+    {
+      question: 'What is 4 * 2?',
+      answers: [
+        { text: '6', correct: false },
+        { text: '8', correct: true }
+      ]
+    }
+  ]
+
 let questionEL, currentQuestion
 
 startButton.addEventListener('click', startGame)
@@ -26,6 +61,8 @@ function startGame() {
   questionsContainer.classList.currentQuestion.remove('hide')
   nextQuestions()
 };
+function reset() 
+showQuestion(questionEl [currentQuestion])
 
 function nextQuestion(question) {
     questionsEl.innerText =    questions.question
@@ -35,25 +72,36 @@ function nextQuestion(question) {
         if (answer.correct) {
             button.dataset.correct = answer.correct
         }
+        button.addEventListener('click', Answerselect)
+        answerEl.append(button)
     })
-    reset() 
-    showQuestion(questionEl [currentQuestion])
-    
-}
+  function reset() {
+      clearClass(document.body)
+      nextButton.classList.add('hide')
+      while (answerEl.firstChild) {
+          answerEl.removeChild(answerEl.firstChild)
+      }
 
+function Answer(e) {
+    const highlightedBtn =e.target
+    const correct =highlightedBtn.dataset.correct
+    setClass(document.body, correct)
+    Array.from(answerEl.children).forEach(button =>{
+        setClass(button,button.dataset.correct)
+    })
+    if (questionEl.clientHeight > current + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Reset'
+        startButton.classList.remove('hide')
+    }
 
-function Question() {
+    function setClass(element, correct) {
+        clearClass(element)
+        if(correct){
+            element.classList.add('correct')
+        } else {
+            element.classList.add('wrong')
 
+        };
 
-
-}
-
-
-
-function Answer() {
-
-
-
-
-
-}
