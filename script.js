2608223ed29a2075d0dd2cd4f813fcb18878d6b5
Kiahna Carmack <Kiahna.carmack@gmail.com>
2021-12-02 17:43:38 -0800

    commit to 'main'

--- script.js
+++ script.js
@@ -1,107 +1,164 @@
 //start the game
 //set the question
 //select answer
+const startButton = document.getElementById('startBtn')
+const nextButton = document.getElementById('nextBtn')
+const questionsContainer = document.getElementById('questions-container')
+const questionEl = document.getElementById('questions')
+const answerBtn = document.getElementById('answerBtn')
 
-const startButton=document.getElementById('start-btn')
-startButton.addEventListener('click', startGame)
-const nextButton= document.getElementById('next-btn')
-const questionContainer = document.getElementById('question-container')
-const questionEl = document.getElementById('question')
-const answerEl = document.getElementById('answerBtn')
+let mixedQuestions,currentQuestion;
+let quizScoreboard =0;
 
-const questions = [
-    {
-      question: 'What is 2 + 2?',
-      answers: [
-        { text: '4', correct: true },
-        { text: '22', correct: false }
-      ]
-    },
-    {
-      question: 'Who is the best YouTuber?',
-      answers: [
-        { text: 'Web Dev Simplified', correct: true },
-        { text: 'Traversy Media', correct: true },
-        { text: 'Dev Ed', correct: true },
-        { text: 'Fun Fun Function', correct: true }
-      ]
-    },
-    {
-      question: 'Is web development fun?',
-      answers: [
-        { text: 'Kinda', correct: false },
-        { text: 'YES!!!', correct: true },
-        { text: 'Um no', correct: false },
-        { text: 'IDK', correct: false }
-      ]
-    },
-    {
-      question: 'What is 4 * 2?',
-      answers: [
-        { text: '6', correct: false },
-        { text: '8', correct: true }
-      ]
+
+function resetState(){
+    clearStatus(document.body)
+    nextButton.classList.add('hide')
+    while(answerBtn.firstChild){
+        answerBtn.removeChild(answerBtn.firstChild)
     }
-  ]
-
-let questionEL, currentQuestion
-
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-  currentQuestion++
-  nextQuestion()
-})
+}
 
 
-function startGame() {
-  console.log('Your Game Has Begun')
-  startButton.classList.add('hide')
-  questionsEl = questions.sort(() => Math.random()- .2)
-  currentQuestion = 0
-  questionsContainer.classList.currentQuestion.remove('hide')
-  nextQuestions()
-};
-function reset() 
-showQuestion(questionEl [currentQuestion])
-
-function nextQuestion(question) {
-    questionsEl.innerText =    questions.question
-    question.answerEl.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.innerTextbutton.classList.add('btnbtn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', Answerselect)
-        answerEl.append(button)
+function quizeAnswer(e){
+    const tagetBtn=e.target
+    const correct=targetBtn.dataset.correct
+    setStatus(document.body,correct)
+    Array.from(answerBtn.childern).forEach(button) => {
+        setStatus,button.dataset.correct)
     })
-  function reset() {
-      clearClass(document.body)
-      nextButton.classList.add('hide')
-      while (answerEl.firstChild) {
-          answerEl.removeChild(answerEl.firstChild)
-      }
-
-function Answer(e) {
-    const highlightedBtn =e.target
-    const correct =highlightedBtn.dataset.correct
-    setClass(document.body, correct)
-    Array.from(answerEl.children).forEach(button =>{
-        setClass(button,button.dataset.correct)
-    })
-    if (questionEl.clientHeight > current + 1) {
-        nextButton.classList.remove('hide')
-    } else {
-        startButton.innerText = 'Reset'
-        startButton.classList.remove('hide')
+    if(mixedQuestions.length > currentQuestion +1){
+        nextButton.classList.remove("hide")
     }
+    else {
+        startButton.innerText="restart"
+        startButton.classList.remove("hide")
+    }
+    if(tagetBtn.dataset = correct) {
+        quizScoreboard++
+}
+document.getElementById('correct-answers').innerText=quizScoreboard
 
-    function setClass(element, correct) {
-        clearClass(element)
-        if(correct){
-            element.classList.add('correct')
-        } else {
-            element.classList.add('wrong')
 
-        };
+
+
+function setStatus(element, correct){
+    clearStatus(element)
+    if(correct){
+        element.classList.add('correct')
+    }
+    else{
+        element.classList.add('wrong')
+    }
+    }
+}
+
+
+function clearStatus(element){
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+const Questions = [ 
+    { question: "What is a stylesheet used when doing web design?",
+		answers: [
+            {text: 'Css', correct: true},
+            {text: 'Javascript',correct: false},
+            {text: 'Css', correct: false},
+            {text: 'Css', correct: false},
+            ]
+			
+		},
+	
+	{
+		question: "What is the Html page used for when creating a webpage?",
+		answers: [
+            {text: 'Local Storage', correct: false},
+            {text: 'Javascript',correct: false},
+            {text: 'basic mockup of page', correct: true},
+            {text: 'Styling', correct: false},
+            ]
+	
+	},
+    {
+		question: "Which link below is used to connect a script.js page and Html page?",
+		answers: [
+            {text: '#script.js', correct: false},
+            {text: '<link src=script.js></link>',correct: false},
+            {text: '@script.js', correct: false},
+            {text: '<script defer src="script.js"></script>', correct: true},
+            ]
+	},
+
+    {
+		question: "What webpage is a great resouce when coding?",
+		answers: [
+            {text: 'WebMd', correct: false},
+            {text: 'MDN',correct: true},
+            {text: 'twitter', correct: false},
+            {text: 'Apple Music', correct: false},
+            ]
+	},
+
+{
+    question: "What is a simple command to save your work (Mac)?",
+    answers: [
+        {text: 'Command + O', correct: false},
+        {text: 'Control + Z',correct: false},
+        {text: 'Command + S', correct: true},
+        {text: 'Control + S', correct: false},
+        ]
+},
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
